e0e42708c5642f1437cbf454f6c5526be63a67ea
MariyaKuznetsova <piratca93@mail.ru>
2025-11-08 01:14:52 +0400

    homework_30_1

--- study/views.py
+++ study/views.py
@@ -1,9 +1,32 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, generics
 
-from study.models import Course
+from study.models import Course, Lesson
+from study.serializers import CourseSerializer, LessonSerializer
 
 
-class CourseViewSet(ModelViewSet):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = LessonSerializer
