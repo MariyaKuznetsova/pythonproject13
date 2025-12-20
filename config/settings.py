8be341b1d0582e3ba9180b44b67dcbb9e35eb7c0
MariyaKuznetsova <piratca93@mail.ru>
2025-12-21 00:05:54 +0400

    homework_35_2

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
-    "drf_yasg" "users",
+    "drf_yasgusers",
     "study",
     "rest_framework_simplejwt",
     "drf_yasg",
